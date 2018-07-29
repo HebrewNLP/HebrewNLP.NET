f506e3f76a8d149f52d53a778ac0c549bbf4c85f
Itay <itay2805@protonmail.com>
2018-07-29 17:18:20 +0300

    Added English-Hebrew name translation with example

---
+++ Name Translation Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using HebrewNLP;
+using System;
+using System.Collections.Generic;
+
+namespace Name_Translation_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HebrewNLP.HebrewNLP.Password = "...";
+
+            List<List<string>> options = NameTranslation.Translate(new String[] { "haim", "haym" }, NameTranslation.Language.HEBREW);
+
+            foreach (List<string> option in options)
+            {
+                Console.WriteLine(option.Count != 0 ? option[0] : null);
+            }
+        }
+    }
+}
